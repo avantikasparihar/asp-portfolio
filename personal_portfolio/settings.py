31a02021f87411f5894903bf308bee2194a657ab
Avantika Singh Parihar <avantikasparihar@gmail.com>
2020-09-12 22:11:07 +0530

    modify settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -124,12 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/img/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
